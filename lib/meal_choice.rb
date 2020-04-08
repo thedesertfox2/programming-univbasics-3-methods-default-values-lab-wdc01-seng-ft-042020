c6606c9e50d469fcde9488d08ae7ed63c106646c
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-08 17:21:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,11 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice ("broccoli", "macaroni")
+veg1 = "broccoli"
+veg2 = "macaroni"
+protein = "tofu"
+
+def meal_choice (veg1, veg2)
   puts "what a nutritious meal!"
 end
-  
+
