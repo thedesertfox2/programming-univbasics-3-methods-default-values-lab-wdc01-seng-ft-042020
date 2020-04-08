f8478f44acc1707d900a5d27a6e73a21fecc2d01
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-08 17:26:31 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,9 +5,9 @@ veg1 = "broccoli"
 veg2 = "carrots"
 protein = "tofu"
 
-def meal_choice (veg1, veg2, protein)
+def meal_choice (veg1 veg2 protein)
   puts "what a nutritious meal!"
   puts "a plate of #{protein} with #{veg1} and #{veg2}"
 end
 
-meal_choice (veg1, veg2, protein)
+meal_choice (veg1 veg2 protein)
